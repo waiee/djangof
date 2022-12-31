60fb54183c875c577b1085e574489b1f0b0e63a9
waiee <waiee_z@yahoo.com>
2022-12-31 22:27:41 +0800

    commit

--- app/models.py
+++ app/models.py
@@ -12,6 +12,6 @@ class Item(models.Model):
     item_id = models.CharField(primary_key=True, max_length=10)
     vendor_id = models.ForeignKey(User, default=None, max_length=10, on_delete=models.CASCADE)
     item_name = models.TextField()
-    item_description = models.TextField(null=True,default=None, blank=True)
+    # item_description = models.TextField(null=True,default=None, blank=True)
     def __str__(self):
         return str(self.item_id)
