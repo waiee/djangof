76e2f023e8559ed90c50d15668d5e607d4afb13a
waiee <waiee_z@yahoo.com>
2022-12-31 22:53:07 +0800

    commit

--- app/models.py
+++ app/models.py
@@ -3,19 +3,17 @@ Definition of models.
 """
 
 from django.db import models
-
 from django.contrib.auth.models import User
 
 #sharing entity
-
 class Vendor(models.Model):
     vendor_id = models.CharField(primary_key=True, max_length=10)
     vendor_name = models.CharField(max_length=100)
     vendor_address = models.CharField(max_length=200)
     vendor_contact = models.BigIntegerField()
-    
+
     def __str__(self):
-        return str(self.item_id)
+        return str(self.vendor_id)
 
 class Item(models.Model):
     item_id = models.CharField(primary_key=True, max_length=10)
