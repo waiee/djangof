1b627681dd043774840ca876011ce92037640184
waiee <waiee_z@yahoo.com>
2023-01-01 22:48:53 +0800

    models

--- app/models.py
+++ app/models.py
@@ -15,12 +15,24 @@ class Staff(models.Model):
 
 class Vendor(models.Model):
     vendor_id = models.CharField(primary_key=True, max_length=10)
-    vendor_name = models.CharField(max_length=100)
-    vendor_address = models.CharField(max_length=200)
-    vendor_contact = models.BigIntegerField()
+    vendor_name = models.CharField(max_length=20)
+    vendor_address = models.CharField(max_length=100)
+    vendor_contact = models.BigIntegerField(max_length=20)
 
     def __str__(self):
         return str(self.vendor_id)
+
+class Item(models.Model):
+    item_id = models.CharField(primary_key=True, max_length=10)
+    vendor_id = models.ForeignKey(Vendor,default=None, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=100)
+    item_name =  models.CharField(max_length=100)
+    item_quantity =  models.BigIntegerField(default=None)
+    item_price = models.BigIntegerField(default=None)
+
+    # item_description = models.TextField(null=True,default=None, blank=True)
+    def __str__(self):
+        return str(self.item_id)
 
 class Item(models.Model):
     item_id = models.CharField(primary_key=True, max_length=10)
