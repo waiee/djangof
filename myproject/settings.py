e6c2cecebe6d2f9607d9a2fc8905cba1027b3ed2
waiee <waiee_z@yahoo.com>
2023-01-04 09:25:43 +0800

    settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -34,10 +34,11 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.messages',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'additem',
 ]
 
 MIDDLEWARE = [
